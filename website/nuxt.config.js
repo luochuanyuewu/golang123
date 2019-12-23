00292f5997c960981c0351a7898f69e67978e940
luochuanyuewu <luochuanyuewu@gmail.com>
2019-12-23 16:09:44 +0800

    eslint和prettier进一步配置

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -1,32 +1,32 @@
 export default {
-    mode: 'universal',
-    head: {
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-    },
-    css: ['iview/dist/styles/iview.css', '~assets/styles/common.css'],
-    loading: { color: '#80bd01' },
-    performance: {
-        prefetch: false
-    },
+  mode: 'universal',
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  css: ['iview/dist/styles/iview.css', '~assets/styles/common.css'],
+  loading: { color: '#80bd01' },
+  performance: {
+    prefetch: false
+  },
 
+  /*
+   ** Build configuration
+   */
+  build: {
     /*
-     ** Build configuration
+     ** You can extend webpack config here
      */
-    build: {
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {}
-    },
-    plugins: [
-        { src: '~plugins/iview.js', ssr: true },
-        { src: '~plugins/bdStat.js', ssr: false },
-        { src: '~plugins/analyze.js', ssr: false },
-        // { src: '~plugins/adsense.js', ssr: false },
-        { src: '~plugins/refreshToken.js', ssr: true }
-    ]
+    extend(config, ctx) {}
+  },
+  plugins: [
+    { src: '~plugins/iview.js', ssr: true },
+    { src: '~plugins/bdStat.js', ssr: false },
+    { src: '~plugins/analyze.js', ssr: false },
+    // { src: '~plugins/adsense.js', ssr: false },
+    { src: '~plugins/refreshToken.js', ssr: true }
+  ]
 }
